814f32b5bf474d08eeb94380b73e027c75eb5755
camisatx <joshschertz3@gmail.com>
2016-04-12 16:51:07 -0500

    Update all files to version 1.3.2

--- pySecMaster/utilities/multithread.py
+++ pySecMaster/utilities/multithread.py
@@ -1,5 +1,30 @@
 from multiprocessing import Pool
 
+__author__ = 'Josh Schertz'
+__copyright__ = 'Copyright (C) 2016 Josh Schertz'
+__description__ = 'An automated system to store and maintain financial data.'
+__email__ = 'josh[AT]joshschertz[DOT]com'
+__license__ = 'GNU AGPLv3'
+__maintainer__ = 'Josh Schertz'
+__status__ = 'Development'
+__url__ = 'https://joshschertz.com/'
+__version__ = '1.3.2'
+
+'''
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 
 def multithread(function, items, threads=4):
     """ Takes the main function to run in parallel, inputs the variable(s)
